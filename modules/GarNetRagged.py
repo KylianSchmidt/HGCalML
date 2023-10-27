c83af35cdcd58ba989d0757b9b1adeb1ef5d7a53
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2023-10-27 14:25:41 +0200

    Working GarNetRagged without aggregation

--- modules/GarNetRagged.py
+++ modules/GarNetRagged.py
@@ -13,8 +13,9 @@ class GarNetRagged(LayerWithMetrics):
         https://arxiv.org/pdf/1902.07987.pdf
 
         Abbreviations for shapes:
-            B : batch size
+            E : number of events
             H : number of hits
+            B : batch size = E*H
             F : number of features
             S : number of aggregators
             P : number of propagators (n_FLR_nodes)
@@ -26,13 +27,16 @@ class GarNetRagged(LayerWithMetrics):
         self.n_FLR_nodes = n_FLR_nodes
 
         with tf.name_scope(self.name+"F_LR"):
-            self.input_feature_transform = tf.keras.layers.Dense(n_FLR_nodes, name="FLR")
+            self.input_feature_transform = tf.keras.layers.Dense(
+                n_FLR_nodes, name="FLR")
 
         with tf.name_scope(self.name+"S"):
-            self.aggregator_distance = tf.keras.layers.Dense(n_aggregators, name="S")
+            self.aggregator_distance = tf.keras.layers.Dense(
+                n_aggregators, name="S")
 
         with tf.name_scope(self.name+"F_out"):
-            self.output_feature_transform = tf.keras.layers.Dense(n_Fout_nodes, activation="tanh", name="F_out")
+            self.output_feature_transform = tf.keras.layers.Dense(
+                n_Fout_nodes, activation="tanh", name="F_out")
 
     def build(self, input_shape):
         input_shape = input_shape[0]
@@ -44,45 +48,45 @@ class GarNetRagged(LayerWithMetrics):
             self.aggregator_distance.build(input_shape)
 
         with tf.name_scope(self.name+"F_out"):
-            # F_out (B*H, F+2*S*P)
+            # F_out (E*H, F+2*S*P)
             self.output_feature_transform.build((
                 input_shape[0],
-                input_shape[1] + 2*self.n_FLR_nodes*self.n_aggregators))
+                input_shape[1] + 1*self.n_FLR_nodes*self.n_aggregators))  # TODO change to 2
 
         super().build(input_shape)
 
     def call(self, inputs):
         x, rs = inputs
         # d distance between H vertices and S aggregators
-        # d = Dense(B*H, F) = (B*H, S)
+        # d = Dense(E*H, F) = (E*H, S)
         distance = self.aggregator_distance(x)
-        # Matrix V(d_jk: (B*H, S)
+        # Matrix V(d_jk: (E*H, S)
         edge_weights = tf.exp(-distance**2)
-        # F_LR: rs(Dense(B*H, F)) = (B*H, P)
+        # F_LR: rs(Dense(E*H, F)) = (E*H, P)
         # Has same shape as x but learned representation therefore new values
         features_LR = self.input_feature_transform(x)
-        # f_tilde: f_ij x V(d_jk) = (B*H, 1, P) x (B*H, S, 1) = (B*H, S, P)
+        # f_tilde: f_ij x V(d_jk) = (E*H, 1, P) x (E*H, S, 1) = (E*H, S, P)
         f_tilde = tf.expand_dims(features_LR, axis=1) * tf.expand_dims(edge_weights, axis=2)
-        # f_tilde: rs(Dense(B*H, S, P)) = (B, H, S, P)
+        # f_tilde: rs(Dense(E*H, S, P)) = (E, H, S, P)
         f_tilde = tf.RaggedTensor.from_row_splits(f_tilde, rs)
-        # Aggregation of f_tilde (B, S, 2*P)
-        f_tilde_aggregated = tf.concat([
-            tf.math.reduce_mean(f_tilde+1E-10, axis=1),
-            tf.math.reduce_max(f_tilde, axis=1)],
-            axis=2)
-        # Return f_updated to the hits: (B, S, 2*P) x (B*H, S, 1)  = (B*H, S, 2*P)
-        f_updated = f_tilde_aggregated * tf.expand_dims(edge_weights, axis=2)
-        # Reshape to (B*H, 2*P*S)
-        f_updated = tf.reshape(
-            f_updated,
-            [tf.shape(f_updated)[0], 2*self.n_FLR_nodes*self.n_aggregators])
-        # Feature vector as (B*H, F+2*P*S)
+        # rs(E*H, S) = (E, H, S)
+        edge_weights = tf.RaggedTensor.from_row_splits(edge_weights, rs)
+        # Return f_updated to the hits: (E, H, S, 2*P) x (E, H, S, 1) = (E, H, S, 2*P)
+        f_updated = f_tilde * tf.expand_dims(edge_weights, axis=3)
+        # Reshape to (E, H, 2*P*S)
+        f_updated = f_updated.merge_dims(2, 3)
+        f_updated = f_updated.merge_dims(0, 1)
+        # Feature vector as (E*H, F+2*P*S)
         f_out = tf.concat(
             [x, f_updated],
             axis=1)
         f_out = self.output_feature_transform(f_out)
 
         return f_out, distance
+
+    def take_mean(self, input: tf.RaggedTensor, axis: int):
+        # (E, H, S, P) -> (E, H, S, P)
+        return 0
 
     def get_config(self):
         config = {
